7782e5ceb9dde09d769db84449ce36eb458cf8fd
Urbain Vaes <u.vaes13@imperial.ac.uk>
2015-07-10 19:02:37 +0100

    Correct bug for multiline prompt

--- fzf-marks.plugin.zsh
+++ fzf-marks.plugin.zsh
@@ -14,17 +14,22 @@ fzfcmd() {
    [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
 }
 
-function jump() {
-    local jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
+function fzf-marks-navigate() {
+    jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
     if [[ -n ${jumpline} ]]; then
-        local jumpdir=$(echo $jumpline | awk '{print $3}')
+        jumpdir=$(echo $jumpline | awk '{print $3}')
         sed -i "\#${jumpline}#d" $BOOKMARKS_FILE
-        cd ${jumpdir} && echo ${jumpline} >> $BOOKMARKS_FILE
+        echo ${jumpline} >> $BOOKMARKS_FILE
+
+        LBUFFER="cd ${jumpdir}"
+        zle && zle redisplay && zle accept-line
     fi
+
+    zle && zle reset-prompt
 }
 
 function dmark()  {
-    local marks_to_delete=$(cat $BOOKMARKS_FILE | $(fzfcmd) -m --bind=ctrl-y:accept,ctrl-t:toggle-up --tac)
+    marks_to_delete=$(cat $BOOKMARKS_FILE | $(fzfcmd) -m --bind=ctrl-y:accept,ctrl-t:toggle-up --tac)
 
     while read -r line; do
         sed -i "\#${line}#d" $BOOKMARKS_FILE
@@ -34,4 +39,4 @@ function dmark()  {
     echo ${marks_to_delete}
 }
 
-bind '"\C-n":"jump\n"'
+zle -N fzf-marks-navigate
