ece3ea960544cb89a69dd418b3a3d8306df17756
Bigo <bigo@crisidev.org>
2017-09-15 11:19:29 +0100

    Properly handle symlinked .bookmarks

--- fzf-marks.plugin.bash
+++ fzf-marks.plugin.bash
@@ -14,22 +14,35 @@ fzfcmd() {
    [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
 }
 
+function handle_symlinks() {
+    local fname
+    if [ -L "${BOOKMARKS_FILE}" ]; then
+        fname=$(readlink "${BOOKMARKS_FILE}")
+    else
+        fname=${BOOKMARKS_FILE}
+    fi
+    echo "${fname}"
+}
+
 function jump() {
-    local jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
+    local jumpline jumpdir bookmarks
+    jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
     if [[ -n ${jumpline} ]]; then
-        local jumpdir=$(echo "${jumpline}" | sed -n "s/.* : \(.*\)$/\1/p" | sed "s#~#$HOME#")
-        perl -p -i -e "s#${jumpline}\n##g" $BOOKMARKS_FILE
+        jumpdir=$(echo "${jumpline}" | sed -n "s/.* : \(.*\)$/\1/p" | sed "s#~#$HOME#")
+        bookmarks=$(handle_symlinks)
+        perl -p -i -e "s#${jumpline}\n##g" "${bookmarks}"
         cd "${jumpdir}" && echo ${jumpline} >> $BOOKMARKS_FILE
     fi
 }
 
 function dmark()  {
-    local marks_to_delete line
+    local marks_to_delete line bookmarks
     marks_to_delete=$(cat $BOOKMARKS_FILE | $(fzfcmd) -m --bind=ctrl-y:accept,ctrl-t:toggle-up --tac)
+    bookmarks=$(handle_symlinks)
 
     if [[ -n ${marks_to_delete} ]]; then
         while read -r line; do
-            perl -p -i -e "s#${line}\n##g" $BOOKMARKS_FILE
+            perl -p -i -e "s#${line}\n##g" ${bookmarks}
         done <<< "$marks_to_delete"
 
         echo "** The following marks were deleted **"
