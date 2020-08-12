4e52e2edf8497cd8c977417d7f460f6229f2f829
Urbain Vaes <urbain@vaes.uk>
2020-08-12 11:15:37 +0200

    Add zsh completion for fzm

--- fzf-marks.plugin.zsh
+++ fzf-marks.plugin.zsh
@@ -165,3 +165,13 @@ bindkey ${FZF_MARKS_JUMP:-'^g'} fzm
 if [ "${FZF_MARKS_DMARK}" ]; then
     bindkey ${FZF_MARKS_DMARK} dmark
 fi
+
+# Completion: for documentation, see e.g.
+# https://mads-hartmann.com/2017/08/06/writing-zsh-completion-scripts.html
+# https://github.com/zsh-users/zsh-completions/blob/master/zsh-completions-howto.org#user-content-actions
+function _fzm {
+    _arguments -C \
+        "1: :(($(sed "s/\\(.*\\) : \\(.*\\)/'\1'\\\\:'\2'/" < ~/.fzf-marks)))" \
+}
+
+compdef _fzm fzm
